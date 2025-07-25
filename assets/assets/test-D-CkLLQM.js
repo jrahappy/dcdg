b1da73227ea4b6c5bb7c46f9207cf2ec2fa94688
jrahappy <william.bh.lee@gmail.com>
2025-07-24 23:58:47 -0400

    first

---
+++ assets/assets/test-D-CkLLQM.js
@@ -0,0 +1,1 @@
+function o(){console.log("This is an important function.")}console.log("Main JavaScript file loaded.");o();
---
+++ static/js/main.js
@@ -0,0 +1,5 @@
+import '../css/main.css';
+import { sayHello } from './important.js';
+
+console.log("Main JavaScript file loaded.");
+sayHello('William');
